6e53673f83e8674e508af27c645745beccadc24e
Jumoc <juanpcaicedo2@gmail.com>
2021-04-15 01:26:53 -0500

    Add script uncomplete

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,41 +1,38 @@
 #!/usr/bin/env bash
-# Install Nginx
-if ! [ -x "$(command -v nginx)" ]
-then
-   apt-get update
-   apt-get install nginx -y
-   service nginx start
+if ! [ -x "$(command -v nginx)" ]; then
+    apt-get -y install nginx
 fi
 
-# Create directories and index.html
-FILE=/data/web_static/releases/test/index.html
-DATA="<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>"
-FILE_DIRECTORY=/data/web_static/releases/test/
-if ! [ -f "$FILE" ]; then
-    mkdir -p "$FILE_DIRECTORY"
-    echo -e  "$DATA" > "$FILE"
+if [[ ! (-d "/data/") ]]; then
+    mkdir "/data/"
 fi
 
-# Create directory shared
-DIRECTORY=/data/web_static/shared/
-if ! [ -d "$DIRECTORY" ]; then
-    mkdir -p "$DIRECTORY"
+if [[ ! (-d "/data/web_static/") ]]; then
+    mkdir "/data/web_static/"
 fi
 
-# Create soft link from current to test directory
-LINK=/data/web_static/current
-TARGET=/data/web_static/releases/test
-ln -sf "$TARGET" "$LINK"
-
-# Change owner
-chown -hR ubuntu:ubuntu /data
-
-# Change Nginx configuration file
-CONFIG_WEB=/etc/nginx/sites-available/default
-DATA="\\\n\tlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t}"
-IS_IN_FILE=$(cat $CONFIG_WEB | grep -c "hbnb_static")
-if [ $IS_IN_FILE -eq 0 ];
-then
-   sed -i "/server_name _;/ a $DATA" "$CONFIG_WEB"
-   nginx -s reload
+if [[ ! (-d "/data/web_static/releases/") ]]; then
+    mkdir "/data/web_static/releases/"
 fi
+
+if [[ ! (-d "/data/web_static/shared/") ]]; then
+    mkdir "/data/web_static/shared/"
+fi
+
+if [[ ! (-d "/data/web_static/releases/test/") ]]; then
+    mkdir "/data/web_static/releases/test/"
+fi
+
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/current /data/web_static/releases/test/
+
+chown -R -H ubuntu:ubuntu /data/
+
+#location=""
