44a684595487b273d100e2de924eee97308b6494
Jumoc <juanpcaicedo2@gmail.com>
2021-04-15 22:11:10 -0500

    Fix: changes the format of the simple html

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,33 +4,33 @@ if ! [ -x "$(command -v nginx)" ]; then
     apt-get -y install nginx
 fi
 
-if [[ ! (-d "/data/") ]]; then
-    mkdir "/data/"
+if [[ ! (-d /data/) ]]; then
+    mkdir /data/
 fi
 
-if [[ ! (-d "/data/web_static/") ]]; then
-    mkdir "/data/web_static/"
+if [[ ! (-d /data/web_static/) ]]; then
+    mkdir /data/web_static/
 fi
 
-if [[ ! (-d "/data/web_static/releases/") ]]; then
-    mkdir "/data/web_static/releases/"
+if [[ ! (-d /data/web_static/releases/) ]]; then
+    mkdir /data/web_static/releases/
 fi
 
-if [[ ! (-d "/data/web_static/shared/") ]]; then
-    mkdir "/data/web_static/shared/"
+if [[ ! (-d /data/web_static/shared/) ]]; then
+    mkdir /data/web_static/shared/
 fi
 
-if [[ ! (-d "/data/web_static/releases/test/") ]]; then
-    mkdir "/data/web_static/releases/test/"
+if [[ ! (-d /data/web_static/releases/test/) ]]; then
+    mkdir /data/web_static/releases/test/
 fi
 
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+echo "<html>" > /data/web_static/releases/test/index.html
+echo "<head>" >> /data/web_static/releases/test/index.html
+echo "</head>" >> /data/web_static/releases/test/index.html
+echo "<body>" >> /data/web_static/releases/test/index.html
+echo "Holberton School" >> /data/web_static/releases/test/index.html
+echo "</body>" >> /data/web_static/releases/test/index.html
+echo "</html>" >> /data/web_static/releases/test/index.html
 
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
