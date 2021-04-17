0ade33b4dc9faa7f3b31e9d4cd0c6ee1a437dbc7
Jumoc <juanpcaicedo2@gmail.com>
2021-04-16 20:30:35 -0500

    Change location

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -37,7 +37,7 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
 location="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
-if [ "$(grep -c 'location /hbnb_static/' /etc/nginx/sites-available/default)" -eq 0 ]; then
+if [ "$(grep -c 'location /hbnb_static' /etc/nginx/sites-available/default)" -eq 0 ]; then
     sed -i "/listen 80 default_server;/a $location" /etc/nginx/sites-available/default
 fi
 
