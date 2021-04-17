0d4737a2b0294fd2a48f3535799cd0573f375ad8
Jumoc <juanpcaicedo2@gmail.com>
2021-04-17 00:08:35 -0500

    Add fabric script to pack everything into versions

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,18 +7,18 @@ from os.path import getsize
 
 def do_pack():
     """packs everything into versions"""
-    local("mkdir -p /versions")
+    local("mkdir -p versions")
     tarname = "web_static_{}.tgz".format(
         datetime.now().strftime("%Y%m%d%H%M%S")
     )
     try:
-        print("Packing web_static to versions/{}.tgz".format(
+        print("Packing web_static to versions/{}".format(
             tarname
         ))
-        local("tar -czvf /versions/{} /data/web_static/".format(tarname))
+        local("tar -czvf versions/{} web_static".format(tarname))
         print("web_static packed: versions/{}.tgz -> {}Bytes".format(
-            tarname, getsize("/versions/{}.tgz".format(tarname))
+            tarname, getsize("versions/{}.tgz".format(tarname))
         ))
-        return "/versions/{}.tgz".format(tarname)
+        return "versions/{}.tgz".format(tarname)
     except:
         return None
