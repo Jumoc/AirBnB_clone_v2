71d373fa60b982bd3a3364faf5d4d84b1a638637
Jumoc <juanpcaicedo2@gmail.com>
2021-04-17 01:18:34 -0500

    Fix: missing permissions and script 1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,11 +5,30 @@ from datetime import datetime
 from os.path import *
 
 
+def do_pack():
+    """packs everything into versions"""
+    local("mkdir -p versions")
+    tarname = "web_static_{}.tgz".format(
+        datetime.now().strftime("%Y%m%d%H%M%S")
+    )
+    try:
+        print("Packing web_static to versions/{}".format(
+            tarname
+        ))
+        local("tar -czvf versions/{} web_static".format(tarname))
+        print("web_static packed: versions/{} -> {}Bytes".format(
+            tarname, getsize("versions/{}".format(tarname))
+        ))
+        return "versions/{}".format(tarname)
+    except:
+        return None
+
+
 def do_deploy(archive_path):
     """packs everything into versions"""
     if not exists(archive_path):
         return False
-    
+
     env.hosts = [
         '54.157.200.176',
         '35.237.198.52',
@@ -19,12 +38,18 @@ def do_deploy(archive_path):
         filename = filename.split(".")[0]
         put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}/".format(filename))
-        run("tar -zxf /tmp/{}.tgz -C /data/web_static/releases/{}".format(filename, filename))
+        run("tar -zxf /tmp/{}.tgz -C /data/web_static/releases/{}".format(
+            filename, filename
+            ))
         run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(filename))
+        run("mv /data/web_static/releases/{}/web_static/*"
+            "/data/web_static/releases/{}/".format(
+                filename
+                ))
         run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current".format())
+        run("ln -s /data/web_static/releases/{}"
+            "/data/web_static/current".format(filename))
         print("New version deployed!")
         return True
     except:
