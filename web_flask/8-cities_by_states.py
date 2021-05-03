0f65ee829e529b1d8eda9616925c60c46efc6e80
Jumoc <juanpcaicedo2@gmail.com>
2021-05-02 23:36:18 -0500

    Test code

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""hello_route module"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def cleanup_storage(e):
+    storage.close()
+
+
+@app.route('/cities_by_states')
+def list_states():
+    states = storage.all(State)
+    for state in states.values():
+        print(state.cities)
+    # return render_template(
+    #    '7-states_list.html', states=[value for value in states.values()]
+    #    )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
